b454dcb7047e5cd51f7bc607519cf60dcb13acb1
inyee786 <intakhab.cusat@gmail.com>
2018-08-15 14:45:29 +0530

    remove the wait

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ http.get(options, function (err, resp, body) {
 const Client = require('kubernetes-client').Client;
 const config = require('kubernetes-client').config;
 const client = new Client({ config: config.fromKubeconfig(), version: '1.9' });
-const namespaces = await client.api.v1.namespaces.get();
+const namespaces = client.api.v1.namespaces.get();
 console.log(namespaces);
 console.log('this is name spaces file')
 
