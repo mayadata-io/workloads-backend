5b01a86e0a488bf2c80120e445a5895015aecd5e
inyee786 <intakhab.cusat@gmail.com>
2018-08-15 14:39:09 +0530

    add ne module

--- app.js
+++ app.js
@@ -66,6 +66,14 @@ http.get(options, function (err, resp, body) {
 
 });
 
+
+const Client = require('kubernetes-client').Client;
+const config = require('kubernetes-client').config;
+const client = new Client({ config: config.fromKubeconfig(), version: '1.9' });
+const namespaces = await client.api.v1.namespaces.get();
+console.log(namespaces);
+console.log('this is name spaces file')
+
 app.use('/sample', api);
 app.use('/person', api1);
 app.use('/',kube);
