15e361b977ee3d69501831738c888297f09113da
inyee786 <intakhab.cusat@gmail.com>
2018-08-15 13:43:23 +0530

    check the namespaces

--- routes/kubernets.js
+++ routes/kubernets.js
@@ -6,13 +6,30 @@ const router = express();
 
 router.get('/', (req, res) => {
 
-    var k8sApi = k8s.Config.defaultClient();
-    k8sApi.listNamespacedPod('default')
-        .then((res) => {
-            console.log(res.body);
-            console.log("this kubernettes");
-        });
+    var namespace = {
+        metadata: {
+          name: 'test'
+        }
+      };
+      
+      k8sApi.createNamespace(namespace).then(
+        (response) => {
+          console.log('Created namespace');
+          console.log(response);
+          k8sApi.readNamespace(namespace.metadata.name).then(
+            (response) => {
+              console.log(response);
+              k8sApi.deleteNamespace(
+                namespace.metadata.name, {} /* delete options */);
+            });
+        },
+        (err) => {
+          console.log('Error!: ' + err);
+        }
+      );
 });
+
+
 
 console.log('this is kube ');
 
