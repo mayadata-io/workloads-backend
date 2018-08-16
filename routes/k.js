486e5c9ed63b9d59510327aaa08519fbfaf2f2e2
inyee786 <intakhab.cusat@gmail.com>
2018-08-16 15:04:31 +0530

    iu

--- routes/k.js
+++ routes/k.js
@@ -6,16 +6,10 @@ kc.loadFromCluster();
 let k8sApi = new k8s.Core_v1Api(kc.getCurrentCluster()['server']);
 k8sApi.setDefaultAuthentication(kc);
 
-// k8sApi.listNamespacedPod('mon')
-//     .then((res) => {
-//         console.log(res.body);
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     });
-
-// var k8sApi = k8s.Config.defaultClient();
 k8sApi.listNamespacedPod('mongo-jiva')
     .then((res) => {
-        console.log(res.body.items);
+	console.log(res.body);
+    })
+    .catch((err) => {
+        console.log(err);
     });
