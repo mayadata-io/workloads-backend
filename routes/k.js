3d2662f0bc4ff90383468e161f57afeca4645445
inyee786 <intakhab.cusat@gmail.com>
2018-08-16 12:55:57 +0530

    config api

--- routes/k.js
+++ routes/k.js
@@ -1,7 +1,7 @@
 const k8s = require('@kubernetes/client-node');
 
 var k8sApi = k8s.Config.defaultClient();
-k8sApi.listNamespacedPod('mongo-jiva')
-   .then((res) => {
-       console.log(res.body.items);
-   });
+k8sApi.listNamespacedPod('default')
+    .then((res) => {
+        console.log(res);
+    });
