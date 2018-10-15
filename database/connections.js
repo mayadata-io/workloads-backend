6fbfc45683760b75a3bd5fe2710219057b406567
inyee786 <intakhab.cusat@gmail.com>
2018-10-15 14:08:03 +0530

    add cockroach database

--- database/connections.js
+++ database/connections.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 
-// const cockroachjiva = require('./cockroach-jiva/databasequery');
+const cockroachjiva = require('./cockroach-jiva/databasequery');
 const mongojiva = require('./mongo-jiva/databasequery');
 const perconajiva = require('./percona-jiva/databasequery');
 const mongocstor = require('./mongo-cstor/databasequery');
 const perconacstor = require('./percona-cstor/databasequery');
 
-// app.use('/cockroach-jiva', cockroachjiva);
+app.use('/cockroach-jiva', cockroachjiva);
 app.use('/mongo-jiva', mongojiva);
 app.use('/percona-jiva', perconajiva);
 app.use('/mongo-cstor', mongocstor);
