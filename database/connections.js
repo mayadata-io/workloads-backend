f194a7a691bd0abbfaecb4a0a1e966f6d9a5e760
inyee786 <intakhab.cusat@gmail.com>
2018-10-15 13:05:07 +0530

    add percona cstor

--- database/connections.js
+++ database/connections.js
@@ -5,12 +5,12 @@ const app = express();
 const mongojiva = require('./mongo-jiva/databasequery');
 const perconajiva = require('./percona-jiva/databasequery');
 const mongocstor = require('./mongo-cstor/databasequery');
-// const perconacstor = require('./percona-cstor/databasequery');
+const perconacstor = require('./percona-cstor/databasequery');
 
 // app.use('/cockroach-jiva', cockroachjiva);
 app.use('/mongo-jiva', mongojiva);
 app.use('/percona-jiva', perconajiva);
 app.use('/mongo-cstor', mongocstor);
-// app.use('/percona-cstor', perconacstor)
+app.use('/percona-cstor', perconacstor)
 module.exports = app;
 
