38218fbc194b2dc09f24c7eb19705e8fa6987781
inyee786 <intakhab.cusat@gmail.com>
2018-10-15 12:21:27 +0530

    add slash for the api

--- database/connections.js
+++ database/connections.js
@@ -7,10 +7,10 @@ const perconajiva = require('./percona-jiva/databasequery');
 const mongocstor = require('./mongo-cstor/databasequery');
 const perconacstor = require('./percona-cstor/databasequery');
 
-app.use('cockroach-jiva', cockroachjiva);
+app.use('/cockroach-jiva', cockroachjiva);
 app.use('/mongo-jiva', mongojiva);
-app.use('percona-jiva', perconajiva);
+app.use('/percona-jiva', perconajiva);
 app.use('/mongo-cstor', mongocstor);
-app.use('percona-cstor', perconacstor)
+app.use('/percona-cstor', perconacstor)
 module.exports = app;
 
