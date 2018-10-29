6a27ac1c3df8d9a28de89eb7e2976b83da41eb6f
bhaskarjb <hcbhaskar7@gmail.com>
2018-10-29 20:20:51 +0530

    data query modify

--- database/redis-jiva/databasequery.js
+++ database/redis-jiva/databasequery.js
@@ -4,40 +4,54 @@ const router = express();
 
 router.post('/save', (req, res) => {
     reddis.dbsize((err,numkeys) => {
-    for (i = 0; i < req.body.length; i++) {
+        let key = numkeys+1;
+    for (i = 0; i<req.body.length; i++) {
+
       
-    let rNumber = i+numkeys;
+    let rNumber = key+i;
     let name = req.body[i].name;
     let email = req.body[i].email;
     let age = req.body[i].age;
+    let val = i+1;
 
    reddis.hmset(rNumber, ['name', name, 'email', email, 'age', age],(err,result) => {
             if(err){
                 console.log(err);
+                res.status(500).json({ status: 500, message: "Data is not saved" });
+
             }else{
-                console.log(result);
-            };
+                if (val==req.body.length) {
+                    res.status(200).json({ status: 200, message: "Data is saved" });
+                }
+                else {console.log(rNumber+'--'+result)};
+                
+            }
+            
       });
    };
-  res.status(200).json({ status: 200, message: "Data is saved" });
- });   
+});   
 });
 
  router.get('/read/:id',(req,res) => {
     reddis.dbsize((err,numkeys) => {
-    for (i = 0; i <numkeys; i++) {
+    for (i = 1; i <=numkeys; i++) {
+        let val=i;
       
-    reddis.hgetall(i ,(err, data) => {
+    reddis.hgetall(i,(err, data) => {
         if(!data){
+            res.status(500).json({ status: 500, message: "Data is not read" });
             console.log('no user exists');
+
         }else{
-            console.log(data)
+            if(val==numkeys){
+            res.status(200).json({ status: 200, message: "Data is read" });
+            }
+            else{console.log(data)};
         };
         
     });
    
     };
-    res.status(200).json({ status: 200, message: "Data is read" });
   });
 }); 
  
