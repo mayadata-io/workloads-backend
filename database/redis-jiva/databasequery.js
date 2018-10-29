0d0d58436bd631ef47a20a81ea9d807cb9fcc6df
bhaskarjb <hcbhaskar7@gmail.com>
2018-10-29 13:15:42 +0530

    replace var with let & quotes chane single

--- database/redis-jiva/databasequery.js
+++ database/redis-jiva/databasequery.js
@@ -6,25 +6,22 @@ router.post('/save', (req, res) => {
     reddis.dbsize( function (err,numkeys) {
     for (i = 0; i < req.body.length; i++) {
       
-    var rNumber = i+numkeys;
-    var name = req.body[i].name;
-    var email = req.body[i].email;
-    var password = req.body[i].age;
-    // console.log(rNumber);
+    let rNumber = i+numkeys;
+    let name = req.body[i].name;
+    let email = req.body[i].email;
+    let age = req.body[i].age;
 
-    // console.log(rNumber,name,email,password);
-    reddis.hmset(rNumber, ["name", name, "email", email, "age", password],function(err,result){
+   reddis.hmset(rNumber, ['name', name, 'email', email, 'age', age],function(err,result){
             if(err){
                 console.log(err);
             }else{
                 console.log(result);
-                // res.render('add-user',{message: "User added successfully!!!!"});   
-            }
+            };
       });
-};
-res.status(200).json({ status: 200, message: "Data is saved" });
-});   
- });
+   };
+  res.status(200).json({ status: 200, message: "Data is saved" });
+ });   
+});
 
  router.get('/read/:id',function(req,res) {
     reddis.dbsize( function (err,numkeys) {
@@ -32,19 +29,16 @@ res.status(200).json({ status: 200, message: "Data is saved" });
       
     reddis.hgetall(i ,function(err, data){
         if(!data){
-            // res.render('users',{message: "No user exist"});
             console.log('no user exists');
         }else{
-            // data.id = id;
             console.log(data)
-            // res.render('users',{users: data});
-        }
+        };
         
     });
    
-};
-res.status(200).json({ status: 200, message: "Data is read" });
-    });
+    };
+    res.status(200).json({ status: 200, message: "Data is read" });
+  });
 }); 
  
 module.exports = router;
