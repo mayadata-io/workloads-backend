06288c05f15585824e880c27b9e8437ed5ae5333
inyee786 <intakhab.ali@mayadata.io>
2019-02-06 08:23:05 +0530

    add minio details in yaml file

--- routes/yaml.js
+++ routes/yaml.js
@@ -187,4 +187,15 @@ router.get('/yaml/nuodb-cstor', (req, res) => {
         workloadyaml:"https://github.com/openebs/litmus/blob/master/apps/nuodb/deployers/nuodb.yaml"
  });
 });
+router.get('/yaml/minio-cstor', (req, res) => {
+    res.status(200).json({ 
+        status: 200, 
+        workloadName: "minio",
+        openebsEngine:"cStor",
+        applicationType:"Deployment",
+        dashboardurl:"https://minio-dashboard.openebs.ci/minio/",
+        nameSpaceyaml: "https://github.com/openebs/e2e-infrastructure/blob/816ae44f1bd9c886ce506a72b542edcb323a50b3/production/mongo-cstor/mongo-cstor-namespace.yaml",
+        workloadyaml:"https://github.com/openebs/openebs/blob/master/k8s/demo/minio/minio.yaml"
+ });
+});
 module.exports = router;
