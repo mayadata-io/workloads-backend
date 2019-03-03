82f2fe16f4104b2c83f34d7b72ffe5c7fa49c032
inyee786 <intakhab.ali@mayadata.io>
2019-03-03 16:30:09 +0530

    add a api for app details

--- routes/kubernetes/kub.js
+++ routes/kubernetes/kub.js
@@ -18,6 +18,7 @@ router.get('/status', (req, resp) => {
     podStatus: []
   };
 
+
   k8sApi.listNamespacedPod(req.query.namespace).then(res => {
     return new Promise(function (resolve, reject) {
       for (i = 0; i < res.body.items.length; i++) {
@@ -67,6 +68,63 @@ router.get('/applications', (request, response) => {
       response.status(200).json(listOfNamespaces);
     });
   });
+});
+
+
+if( process.env.APP_NAME == undefined ){
+    console.error( "provide app name");
+  }else{
+    listOfApp =  process.env.APP_NAME.split(" ");
+  
+    requests = listOfApp.map(name => {
+      var overAllStatus = "";
+      var overAllStatusCount = 0;
+      var status = {
+        Running: 0,
+        Pending: 1,
+        Failed: 2,
+        Unknown: 3
+      };
+      var allStatus = {
+        status: String,
+        appName: String,
+        podStatus: []
+      };
+      return k8sApi.listNamespacedPod(`${name}`).then(res => {
+        return new Promise(function (resolve, reject) {
+          for (i = 0; i < res.body.items.length; i++) {
+            allStatus.name = res.body.items[0].metadata.labels.name;
+            allStatus.namespace = `${name}`;
+            allStatus.apiurl = 'https://workloads.openebs.ci/'
+            
+            if(res.body.items[0].metadata.ownerReferences[0].kind == 'StatefulSet' && res.body.items[0].metadata.ownerReferences[0].kind !== 'undefined'){
+              allStatus.kind = res.body.items[0].metadata.ownerReferences[0].kind;	
+            }else{
+              allStatus.kind = 'Deployment';	
+            }	
+            if (status[res.body.items[i].status.phase] >= overAllStatusCount) {
+              overAllStatusCount = res.body.items[i].status.phase;
+              allStatus.status = res.body.items[i].status.phase;
+          
+            }
+            allStatus.podStatus.push({
+              name: res.body.items[i].metadata.name,
+              status: res.body.items[i].status.phase,
+            });
+          }
+          resolve(allStatus);
+      })
+      })
+    });
+}
+
+
+router.get('/statuses', (request,resp)=>{
+  Promise.all(requests)
+  .then(responses => {  
+    // console.log(JSON.stringify(responses));
+    resp.status(200).json(responses);
+  })
 });
 
 module.exports = router;
