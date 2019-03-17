157cde3543ee519a0d33168989cb60a70f0656b4
inyee786 <intakhab.ali@mayadata.io>
2019-03-18 00:13:31 +0530

    add docker login in push.sh file
     Signed-off-by: inyee786 <intakhab.ali@mayadata.io>

--- push.sh
+++ push.sh
@@ -8,10 +8,12 @@ echo ${Aggregator_Repo}
 echo ${TAG}
 echo ${TRAVIS_BRANCH} ${TRAVIS_PULL_REQUEST}
 
+docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
+
 if [ ${TRAVIS_BRANCH} = "master" -a ${TRAVIS_PULL_REQUEST} = "false" ]
 then
  docker push ${Aggregator_Repo}:${TAG} 
- docker push ${Agent_Repo}:${TAG}                                                                                 docker push $(Agent-Repo):$(TAG)
+ docker push ${Agent_Repo}:${TAG}                                                                             docker push $(Agent-Repo):$(TAG)
 else
   echo "build is successfull"
 fi
